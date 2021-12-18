75ae801d7fc53502038d4b18b043feee682c74a4
Joshua Hans <66792834+jhans404@users.noreply.github.com>
2021-12-18 19:17:19 +0000

    part one finished

--- src/components/product.js
+++ src/components/product.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Product(props) {
   return (
     <div className="Product" >
-      <h2>NAME</h2>
-      <h3>$$PRICE</h3>
+      <h2>{props.name}</h2>
+      <h3>{props.price}</h3>
       <div className="buttons">
         <button onClick={()=>{console.log("add button test")}}>Add</button>
         <button>Remove</button>
--- src/components/productList.js
+++ src/components/productList.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './productList.css';
+import Product from './product';
 // Be sure to uncomment this line before trying to use the Product component!
 // import Product from './product'
 
 function ProductList(props) {
   return (
     <div className="ProductList">
-      Replace this placeholder text with some products
+      {props.inventory.map(item => 
+        <Product name = {item.name} 
+                 price = {item.price}
+        />) 
+      }
     </div>
   );
 }
